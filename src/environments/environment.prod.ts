a69308deb9d80d25522ba3cfb936f0cbf05f029f
Olivier <leplus.olivier@gmail.com>
2018-03-11 02:33:44 +0100

    fix(firebase): Add firebase prod details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCyuB1SPxUdwZ2Cb4raHUT1e8-wvdU05XQ',
+    authDomain: 'lesmirettesvb.firebaseapp.com',
+    databaseURL: 'https://lesmirettesvb.firebaseio.com',
+    projectId: 'lesmirettesvb',
+    storageBucket: 'lesmirettesvb.appspot.com',
+    messagingSenderId: '2798593422'
+  }
 };
